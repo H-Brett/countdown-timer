01ce199fabfb72f36b885f9ad332628e2e53199c
Brett <horst.brett@gmail.com>
2021-05-12 18:59:56 -0600

    functioning countdown, super basic styling

---
+++ index.js
@@ -0,0 +1,19 @@
+let [ day, hour, min, sec ] = document.querySelectorAll('.timer'); 
+
+setInterval( () => {
+    sec.innerText -= 1;
+
+    if(sec.innerText < 0) {
+        min.innerText -= 1; 
+        sec.innerText = 59
+        if (min.innerText < 0) {
+            hour.innerText -= 1; 
+            min.innerText = 59; 
+            if (hour.innerText < 0) {
+                day.innerText -= 1; 
+                hour.innerText = 23; 
+            }
+        }
+    }
+
+}, 1000)
